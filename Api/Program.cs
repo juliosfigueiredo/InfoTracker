8bbce4fcf13b42305a589222f6e59247d394f0a2
Danilo Cavalcante <danilo.cavalcante@adm.unip.br>
2023-06-21 14:16:25 -0300

    retornando program.cs para o padrão

--- Api/Program.cs
+++ Api/Program.cs
@@ -14,12 +14,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-app.UseSwagger();
-app.UseSwaggerUI();
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
